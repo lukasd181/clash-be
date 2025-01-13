9e88e2c1978f03379b3dfad13a99810925b512c7
lukasd181 <hungduonggia181@gmail.com>
2025-01-13 17:56:44 -0500

    finished mongodb connection + dotenv setup

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { authRouter } = require("./routes/auth.api");
+const { indexRouter } = require("./routes/index");
 const { sendResponse } = require("./helpers");
-
-const PORT = 3000;
+const { MONGO_URI, PORT } = process.env;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", authRouter);
+mongoose
+  .connect(MONGO_URI)
+  .then(console.log(`Successfully connected to db: ${MONGO_URI}`))
+  .catch((err) => {
+    console.log(`MongoDB Connection error: ${err}`);
+  });
+
+app.use("/api", indexRouter);
 
 // catch 404, forward to error handler
 app.use((req, res, next) => {
@@ -20,7 +28,6 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// Initialize Error Handling
 app.use((err, req, res, next) => {
   console.log("ERROR: ", err);
   if (err.isOperational) {
